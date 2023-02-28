b0515d47ba2fc45c9382d6cc459dab5802b32873
akhil babu o <akhil.babu@marketsimplified.com>
2023-02-28 23:48:20 +0530

    test

--- app/src/main/java/com/blackbird/diary/common/AppConstants.kt
+++ app/src/main/java/com/blackbird/diary/common/AppConstants.kt
@@ -4,4 +4,5 @@ object AppConstants {
     const val pref_name = "app_preference_for_this"
     const val pin_key = "pin_key_for_this"
     const val isPinSetKey = "isPinSetKey"
+    const val user = ""
 }
